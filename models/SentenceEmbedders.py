c7a83196e8eae493022ee6f5364718b17460e4dd
foksly <mitya1510@ya.ru>
2019-04-20 14:23:07 +0300

    minor fixes

--- models/SentenceEmbedders.py
+++ models/SentenceEmbedders.py
@@ -41,10 +41,6 @@ def ELMoMultipleChoiceEmbedder(elmo,
                 answer_choices_cols[i]].values
         correct_answers_data = dataframe[correct_answer_col]
 
-        answer_choices = {}
-        for i in range(len(answer_choices_cols)):
-            answer_choices[f'choice_{i}'] = dataframe[
-                answer_choices_cols[i]]
     else:
         question_data = kwargs['questions']
         num_choices = len(kwargs['answer_choices'])
@@ -92,12 +88,70 @@ def ELMoMultipleChoiceEmbedder(elmo,
             embeddings.append(sample)
     return embeddings
 
-
-def BertMultipleChoiceEmbedder(bert_encoder,
+def ELMoParaphraseEmbedder(elmo,
                 dataframe=None,
                 batch_size=64,
+                tokenizer=None,
                 mean=False,
                 **kwargs):
+    try:
+        num_samples = len(dataframe)
+    except Exception:
+        try:
+            num_samples = len(kwargs['text_1'])
+        except Exception:
+            raise TypeError('Questions are provided in unsupported format')
+
+    if not tokenizer:
+        tokenizer = RegexpTokenizer('[А-Яа-яёA-Za-z0-9]+')
+    if dataframe is not None:
+        text_1_col = kwargs['text_1_col']
+        text_2_col = kwargs['text_2_col']
+        correct_answer_col = kwargs['correct_answer_col']
+
+        text_1_data = dataframe[text_1_col].values
+        text_2_data = dataframe[text_2_col].values
+        correct_answers_data = dataframe[correct_answer_col]
+
+    else:
+        text_1_data = kwargs['text_1']
+        text_2_data = kwargs['text_2']
+        correct_answers_data = kwargs['correct_answers']
+
+    embeddings = []
+    for batch in tqdm(range(0, num_samples - batch_size, batch_size)):
+        ids = []
+        text_1 = []
+        text_2 = []
+        correct_answers = []
+
+        for i in range(batch, batch + batch_size):
+            ids.append(i)
+            text_1.append(tokenizer.tokenize(text_1[i]))
+            text_2.append(tokenizer.tokenize(text_2[i]))
+            correct_answers.append(correct_answers_data[i])
+
+        text_1_elmo_batch = elmo(text_1)
+        text_2_elmo_batch = elmo(text_2)
+        for j in range(len(text_1_elmo_batch)):
+            if mean:
+                sample = {
+                    'text_1': np.mean(text_1_elmo_batch[j], axis=0),
+                    'text_2': np.mean(text_2_elmo_batch[j], axis=0)
+                }
+            else:
+                sample = {'text_1': text_1_elmo_batch[j], 'text_2': text_2_elmo_batch[j]}
+            sample['id'] = ids[j]
+            sample['correct_answer'] = correct_answers[j]
+            embeddings.append(sample)
+    return embeddings
+
+
+def BertMultipleChoiceEmbedder(bert_encoder,
+                               dataframe=None,
+                               batch_size=64,
+                               mean=False,
+                               **kwargs):
     try:
         num_samples = len(dataframe)
     except Exception:
@@ -155,6 +209,64 @@ def BertMultipleChoiceEmbedder(bert_encoder,
             sample = {'question': questions_bert_batch[j]}
             for choice in choices_bert_batch:
                 sample[choice] = choices_bert_batch[choice][j]
+            sample['id'] = ids[j]
+            sample['correct_answer'] = correct_answers[j]
+            embeddings.append(sample)
+    return embeddings
+
+def BertParaphraseEmbedder(bert_encoder,
+                           dataframe=None,
+                           batch_size=64,
+                           tokenizer=None,
+                           mean=False,
+                           **kwargs):
+    try:
+        num_samples = len(dataframe)
+    except Exception:
+        try:
+            num_samples = len(kwargs['text_1'])
+        except Exception:
+            raise TypeError('Questions are provided in unsupported format')
+
+    if not tokenizer:
+        tokenizer = RegexpTokenizer('[А-Яа-яёA-Za-z0-9]+')
+    if dataframe is not None:
+        text_1_col = kwargs['text_1_col']
+        text_2_col = kwargs['text_2_col']
+        correct_answer_col = kwargs['correct_answer_col']
+
+        text_1_data = dataframe[text_1_col].values
+        text_2_data = dataframe[text_2_col].values
+        correct_answers_data = dataframe[correct_answer_col]
+
+    else:
+        text_1_data = kwargs['text_1']
+        text_2_data = kwargs['text_2']
+        correct_answers_data = kwargs['correct_answers']
+
+    embeddings = []
+    for batch in tqdm(range(0, num_samples - batch_size, batch_size)):
+        ids = []
+        text_1 = []
+        text_2 = []
+        correct_answers = []
+
+        for i in range(batch, batch + batch_size):
+            ids.append(i)
+            text_1.append(text_1[i])
+            text_2.append(text_2[i])
+            correct_answers.append(correct_answers_data[i])
+
+        text_1_elmo_batch = bert_encoder(text_1)
+        text_2_elmo_batch = bert_encoder(text_2)
+        for j in range(len(text_1_elmo_batch)):
+            if mean:
+                sample = {
+                    'text_1': np.mean(text_1_elmo_batch[j], axis=0),
+                    'text_2': np.mean(text_2_elmo_batch[j], axis=0)
+                }
+            else:
+                sample = {'text_1': text_1_elmo_batch[j], 'text_2': text_2_elmo_batch[j]}
             sample['id'] = ids[j]
             sample['correct_answer'] = correct_answers[j]
             embeddings.append(sample)
